81e21498a2555662e59d375a5f55595e9f1891a4
Emily Ellison <emilyellison986@gmail.com>
2012-05-23 17:01:23 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamBeerOnInfo::Application.config.secret_token = '9a0aee783342eaa3b6897087f1702fd51b30b68a00d5e59dc5506e4e1f9ab50505b91a509e7ca83334b15b727457fc5c6f7f865f72d7771c96f987a4e2103d14'
